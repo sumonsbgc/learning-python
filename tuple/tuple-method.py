5d638d394d2aa80e15eb598067b6e88b1f5ca94b
Mohammad Sumon <sumonsbgc@gmail.com>
2023-03-01 18:53:51 +0600

    creating new branch tuple

---
+++ tuple/tuple-method.py
@@ -0,0 +1,19 @@
+users = ["Jhon Doe", "Karim", "Rahim", "Jamal", "kamal", "Matin", "Halim"]
+
+
+""" len() """
+""" count() """
+
+print(len(users))
+print(users.count("Messi"))
+
+# users.append("Kalam")
+# users.insert(1, "Asif")
+# users.extend(["Neimar", "Messi"])
+# users.remove("Karim Benzama")
+# del users[-1]
+# users.pop()
+# users.pop(3)
+# print(users.count("Messi"))
+# users.reverse()
+# users.sort()
---
+++ tuple/tuple.py
@@ -0,0 +1,27 @@
+"""
+-> Tuple (kind of list)
+-> use ()
+-> can't be update, remove, and add new item.
+-> mixed data type
+-> can be used any data type, list, tuple, dictionary etc
+-> can be added new item with comma after the item.
+"""
+
+users = ("Jhon Doe", "karim", "Rahim", "Jamal", "kamal", "Matin", "Halim")
+print(type(users));
+print(users);
+
+""" Access of Tuple Item """
+# Print First Item
+print(users[0]);
+
+# Print Last Item
+print(users[-1]);
+
+# Print Last Item
+print(users[2:6]);
+
+""" Update List Item """
+# Update not possible but
+""" Manipulation List """
+""" Remove and Delete """
